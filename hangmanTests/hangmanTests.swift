72aa9b20c32135127fd419aaaccd03dff5fab6c1
just.ernst <ernst.r.fanfan@gmail.com>
2025-03-09 10:21:41 -0400

    Initial Commit

---
+++ hangmanTests/hangmanTests.swift
@@ -0,0 +1,17 @@
+//
+//  hangmanTests.swift
+//  hangmanTests
+//
+//  Created by Ernst Fanfan on 3/9/25.
+//
+
+import Testing
+@testable import hangman
+
+struct hangmanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
